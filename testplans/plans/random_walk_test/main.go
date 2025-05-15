c653512bc9925ee148068ea8b38e8ecb585cacc8
Erik Gugumus <89041732+erik9876@users.noreply.github.com>
2025-05-15 20:25:29 +0200

    add random walk testplan

---
+++ testplans/plans/random_walk_test/main.go
@@ -0,0 +1,171 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	dht "github.com/erik9876/go-libp2p-kad-dht"
+	logging "github.com/ipfs/go-log"
+	libp2p "github.com/libp2p/go-libp2p"
+	crypto "github.com/libp2p/go-libp2p/core/crypto"
+	"github.com/libp2p/go-libp2p/core/peer"
+	"github.com/testground/sdk-go/run"
+	"github.com/testground/sdk-go/runtime"
+	"github.com/testground/sdk-go/sync"
+)
+
+func main() {
+    run.InvokeMap(map[string]interface{}{
+        "random_walk_test": run.InitializedTestCaseFn(runRandomWalkTest),
+    })
+}
+
+func runRandomWalkTest(runenv *runtime.RunEnv, initCtx *run.InitContext) error {
+    logging.SetAllLoggers(logging.LevelInfo)
+    if err := logging.SetLogLevel("dht", "info"); err != nil {
+        fmt.Println("Logging error:", err)
+    }
+    if err := logging.SetLogLevel("dht/RtRefreshManager", "error"); err != nil {
+        fmt.Println("Logging error:", err)
+    }
+    if err := logging.SetLogLevel("rtrefresh", "error"); err != nil {
+        fmt.Println("Logging error:", err)
+    }
+
+    ctx, cancel := context.WithCancel(context.Background())
+    defer cancel()
+
+    client := sync.MustBoundClient(ctx, runenv)
+    defer client.Close()
+
+    seq := client.MustSignalEntry(ctx, sync.State("enrolled"))
+    initCtx.MustWaitAllInstancesInitialized(ctx)
+    barrier := func(state string) {
+        if _, err := client.SignalAndWait(ctx, sync.State(state), runenv.TestInstanceCount); err != nil {
+            runenv.RecordMessage("error waiting for %s: %s", state, err)
+        }
+    }
+
+    h, err := libp2p.New(libp2p.ListenAddrStrings("/ip4/0.0.0.0/tcp/0"))
+    if err != nil {
+        return fmt.Errorf("libp2p.New failed: %w", err)
+    }
+    defer h.Close()
+
+    barrier("initialized")
+
+    kadDHT, err := dht.New(ctx, h, dht.Mode(dht.ModeServer))
+    if err != nil {
+        return fmt.Errorf("creating DHT failed: %w", err)
+    }
+    defer kadDHT.Close()
+
+    topicPeers := sync.NewTopic("peers", &peer.AddrInfo{})
+    me := peer.AddrInfo{ID: h.ID(), Addrs: h.Addrs()}
+    if _, err := client.Publish(ctx, topicPeers, &me); err != nil {
+        return fmt.Errorf("publish peers failed: %w", err)
+    }
+    runenv.RecordMessage("I am peer %s", h.ID())
+    barrier("published")
+
+    peerCh := make(chan *peer.AddrInfo, runenv.TestInstanceCount)
+    if _, err := client.Subscribe(ctx, topicPeers, peerCh); err != nil {
+        return fmt.Errorf("subscribe peers failed: %w", err)
+    }
+    var peers []peer.AddrInfo
+    seen := map[peer.ID]bool{}
+    for len(peers) < runenv.TestInstanceCount-1 {
+        select {
+        case p := <-peerCh:
+            if p.ID != h.ID() && !seen[p.ID] {
+                peers = append(peers, *p)
+                seen[p.ID] = true
+                runenv.RecordMessage("Discovered peer %s", p.ID)
+            }
+        case <-time.After(5 * time.Second):
+            runenv.RecordMessage("Timeout collecting peers")
+            break
+        }
+    }
+
+    for _, p := range peers {
+        ctxc, cancelc := context.WithTimeout(ctx, 5*time.Second)
+        if err := h.Connect(ctxc, p); err != nil {
+            runenv.RecordMessage("Connect to %s failed: %s", p.ID, err)
+        }
+        cancelc()
+    }
+    if err := kadDHT.Bootstrap(ctx); err != nil {
+        runenv.RecordMessage("Bootstrap failed: %s", err)
+    } else {
+        runenv.RecordMessage("DHT bootstrap successful")
+    }
+
+    deadline := time.Now().Add(20 * time.Second)
+    for time.Now().Before(deadline) {
+        sz := kadDHT.RoutingTable().Size()
+        runenv.RecordMessage("Routing table size: %d", sz)
+        if sz >= runenv.TestInstanceCount-1 {
+            break
+        }
+        time.Sleep(1 * time.Second)
+    }
+    runenv.RecordMessage("Final routing table size: %d", kadDHT.RoutingTable().Size())
+
+	barrier("routing_table_ready")
+
+    type transferKey struct{ Key string }
+    keyTopic := sync.NewTopic("dht-key", &transferKey{})
+
+    if seq == 1 {
+        runenv.RecordMessage("putting value")
+
+        dhtKey := "/pk/" + h.ID().String()
+
+        rawPub, err := crypto.MarshalPublicKey(h.Peerstore().PubKey(h.ID()))
+        if err != nil {
+            return fmt.Errorf("marshal public key failed: %w", err)
+        }
+
+        if err := kadDHT.PutValue(ctx, dhtKey, rawPub); err != nil {
+            runenv.RecordMessage("PutValue failed: %s", err)
+        } else {
+            runenv.RecordMessage("PutValue successful")
+        }
+
+        if _, err := client.Publish(ctx, keyTopic, &transferKey{dhtKey}); err != nil {
+            return fmt.Errorf("publish dht-key failed: %w", err)
+        }
+    }
+
+    barrier("put_value")
+
+    if seq == 2 {
+        runenv.RecordMessage("starting lookup")
+
+        kch := make(chan *transferKey, 1)
+        if _, err := client.Subscribe(ctx, keyTopic, kch); err != nil {
+            return fmt.Errorf("subscribe dht-key failed: %w", err)
+        }
+        tk := <-kch
+
+		wantCtx, wantCancel := context.WithCancel(context.Background())
+		defer wantCancel()
+        val, err := kadDHT.WantValueFromPeers(wantCtx, tk.Key, 1)
+        if err != nil {
+            runenv.RecordMessage("lookup failed: %s", err)
+        } else {
+            pk2, err := crypto.UnmarshalPublicKey(val)
+            if err != nil {
+                runenv.RecordMessage("unmarshal public key failed: %s", err)
+            } else {
+                runenv.RecordMessage("got pubkey: %s", pk2)
+            }
+        }
+    }
+
+    barrier("done")
+    runenv.RecordSuccess()
+    return nil
+}
